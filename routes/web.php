a870328ede6d46cb4268a04b69817a10c87e406d
Thawid <tanvir.aleshasolutions@gmail.com>
2025-09-15 14:59:53 +0600

    Simple change in breadcumb

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,9 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified',SetPageTitle::class])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware(['auth',CheckDomain::class, SetPageTitle::class])->group(function () {
+Route::middleware(['auth','verified', CheckDomain::class, SetPageTitle::class])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
